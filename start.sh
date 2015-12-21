c363e40a5227de4fc2498d57647322e1ab20f224
haoran.yanghr <haoran.yanghr@alibaba-inc.com>
2015-12-21 21:15:58 +0800

    Remove sudo in start.sh

--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@ SCRIPTPATH=`dirname "${BASH_SOURCE[0]}"`
 cd $SCRIPTPATH
 
 if hash python2 2>/dev/null; then
-    sudo python2 launcher/start.py
+    python2 launcher/start.py
 else
-    sudo python launcher/start.py
+    python launcher/start.py
 fi
